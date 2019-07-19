67b7d6aee3f479c20f9d8755a04acbe271b6296d
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-07-19 20:05:39 +0200

    switched db to postgres and added search bar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,12 @@
 class MoviesController < ApplicationController
   def index
     if current_user != nil
-      @movies = Movie.order(:title).page(params[:page])
-      @watch = Watch.new
+      if params[:query].present?
+        @movies = Movie.search_by_title_and_year(params[:query]).page(params[:page])
+      else
+        @movies = Movie.order(:title).page(params[:page])
+      end
+        @watch = Watch.new
     else
       redirect_to new_user_session_path
     end
@@ -12,4 +16,10 @@ class MoviesController < ApplicationController
     @like = Like.new
   end
 end
+
+
+    # if params[:query].present?
+    #   @houses = House.search_by_address(params[:query])
+    # else
+    #   @houses = House.where.not(latitude: nil, longitude: nil).order(created_at: :desc)
 
