3b9d2eaf292475d700d08a5355e8640732c352da
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-07-20 15:01:03 +0200

    genre num_ids convertable to names

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,8 @@ class Movie < ApplicationRecord
   has_many :users, through: :likes
   has_many :users, through: :watches
 
+  serialize :genre, Array
+
   paginates_per 15
 
   include PgSearch
@@ -12,4 +14,49 @@ class Movie < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
+  def genre_name
+    genre.map do |num|
+      case num
+      when 28
+        "Action"
+      when 12
+        "Adventure"
+      when 16
+        "Animation"
+      when 35
+        "Comedy"
+      when 80
+        "Crime"
+      when 99
+        "Documentary"
+      when 18
+        "Drama"
+      when 10751
+        "Family"
+      when 14
+        "Fantasy"
+      when 36
+        "History"
+      when 27
+        "Horror"
+      when 10402
+        "Music"
+      when 9648
+        "Mystery"
+      when 10749
+        "Romance"
+      when 878
+        "Science Fiction"
+      when 10770
+        "TV Movie"
+      when 53
+        "Thriller"
+      when 10752
+        "War"
+      when 37
+        "Western"
+      end
+    end
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ require 'json'
 
 Movie.destroy_all
 
-(1..200).each do |num|
+(1..10).each do |num|
   url = "https://api.themoviedb.org/3/discover/movie?api_key=49da6ec6d7e005696bf13be889ab5bc9&sort_by=popularity.desc&page=#{num}"
   json = open(url).read
   objs = JSON.parse(json)
 
   objs["results"].each do |movie|
-    Movie.create(title: movie["title"], year: movie["release_date"], rating: movie["vote_average"], plot: movie["overview"])
+    Movie.create(title: movie["title"], year: movie["release_date"], rating: movie["vote_average"], plot: movie["overview"], genre: movie["genre_ids"])
   end
   puts "created page #{num}"
 end
