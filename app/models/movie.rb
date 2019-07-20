d1dcd3aa5960363586701902bfff47479de80dac
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-07-20 13:46:22 +0200

    pagination changed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,8 @@ class Movie < ApplicationRecord
   has_many :users, through: :likes
   has_many :users, through: :watches
 
+  paginates_per 15
+
   include PgSearch
   pg_search_scope :search_by_title_and_year,
     against: [ :title, :year ],
