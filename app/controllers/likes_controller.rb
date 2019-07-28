94261fa59276812934886574dd9799d9e0b633dd
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-07-28 18:46:00 +0200

    fixes to watch and likes controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,21 @@
 class LikesController < ApplicationController
   def create
-    @movie = Movie.find(params[:movie_id])
-    @like = Like.new(like_params)
-    @like.movie = @movie
-    @like.user = current_user
-    @like.save
+    unless current_user.likes.where(movie_id: params[:movie_id]).exists?
+      @movie = Movie.find(params[:movie_id])
+      @like = Like.new(like_params)
+      @like.movie = @movie
+      @like.user = current_user
+      @like.save
 
-    return unless (@movie.watches & current_user.watches).empty?
+      return unless (@movie.watches & current_user.watches).empty?
 
-    @watch = Watch.new
-    @watch.movie = @movie
-    @watch.user = current_user
-    @watch.save
+      @watch = Watch.new
+      @watch.movie = @movie
+      @watch.user = current_user
+      @watch.save
+    # else
+    #   do something
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :watches, :likes
   end
 
+  resources :users, only: [:show]
+
   get '/simusers', to: 'movies#affiliate_users', as: 'affiliate_users'
   get '/watchmovies', to: 'movies#watchable_movies', as: 'watchable_movies'
 end
