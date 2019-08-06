aec5a425fd013793398a22aec0916ae4db5b03b4
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-08-06 22:15:31 +0200

    first changes to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @movies = @user.likes.where(stars: 3).order(:created_at).reverse.map { |like| Movie.find(like[:movie_id]) }
   end
 end
